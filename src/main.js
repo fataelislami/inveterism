9a06910f767f39d85ad4efdeecd5ed3e81df4e02
fataelislami <fataelislami97@gmail.com>
2021-08-04 11:54:59 +0700

    Add section4 with slick

--- src/main.js
+++ src/main.js
@@ -2,16 +2,15 @@ import $ from "jquery";
 import "slick-carousel";
 
 // Slick
-$(document).ready(()=>{
-    // $("#slickTab1").slick(
-    //     {
-    //         dots: true,
-    //         autoplay:true,
-    //         customPaging: function (slider, i) {
-    //             var thumb = $(slider.$slides[i]).data();
-    //             console.log(thumb)
-    //             return '<a class="rounded-full w-8 h-8 border-2 border-gray-100 flex justify-center items-center text-gray-500">'+(i+1)+'</a>';
-    //         }
-    //     }
-    // );
+$(document).ready(() => {
+    $(".slick-testimonials").slick(
+        {
+            dots: true,
+            centerMode: true,
+            autoplay: true,
+            arrows: true,
+            prevArrow: $('.prevTesti'),
+            nextArrow: $('.nextTesti'),
+        }
+    );
 })
