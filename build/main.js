dc7984c5b4d6a01640d8cbd3eaf9bf05d2c9ad7d
fataelislami <fataelislami97@gmail.com>
2021-08-04 12:28:34 +0700

    Release v1.0.0

--- build/main.js
+++ build/main.js
@@ -2,46 +2,26 @@ import $ from "./_snowpack/pkg/jquery.js";
 import "./_snowpack/pkg/slick-carousel.js";
 
 // Slick
-$(document).ready(()=>{
-    $("#slickTab1").slick(
+$(document).ready(() => {
+    $(".slick-testimonials").slick(
         {
             dots: true,
-            autoplay:true,
-            customPaging: function (slider, i) {
-                var thumb = $(slider.$slides[i]).data();
-                console.log(thumb)
-                return '<a class="rounded-full w-8 h-8 border-2 border-gray-100 flex justify-center items-center text-gray-500">'+(i+1)+'</a>';
-            }
+            centerMode: true,
+            autoplay: true,
+            arrows: true,
+            prevArrow: $('.prevTesti'),
+            nextArrow: $('.nextTesti'),
+        }
+    );
+    $(".slick-partners").slick(
+        {
+            dots: true,
+            centerMode: true,
+            autoplay: true,
+            slidesToShow:3,
+            arrows: true,
+            prevArrow: $('.prevPartner'),
+            nextArrow: $('.nextPartner'),
         }
     );
 })
-
-// TABS
-let tabsContainer = document.querySelector("#tabs");
-
-let tabTogglers = tabsContainer.querySelectorAll("a");
-console.log(tabTogglers);
-
-tabTogglers.forEach(function (toggler) {
-    toggler.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        let tabName = this.getAttribute("href");
-
-        let tabContents = document.querySelector("#tab-contents");
-
-        for (let i = 0; i < tabContents.children.length; i++) {
-
-            tabTogglers[i].parentElement.classList.remove("border-green-400", "border-b", "-mb-px", "opacity-100"); tabContents.children[i].classList.remove("hidden");
-            if ("#" + tabContents.children[i].id === tabName) {
-                continue;
-            }
-            tabContents.children[i].classList.add("hidden");
-
-        }
-        e.target.parentElement.classList.add("border-green-400", "border-b-4", "-mb-px", "opacity-100");
-    });
-});
-
-document.getElementById("default-tab").click();
-
